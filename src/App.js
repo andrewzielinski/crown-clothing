e2a688fe7f237eb79a85ff1e9d1eb2f4a1786934
Andrew Zielinski <andrew@2dev.com.au>
2020-10-29 21:41:40 +1100

    Added menu item

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from "react"
 import './App.css'
 import {Switch, Route} from "react-router-dom"
 import ShopPage from "./pages/ShopPage"
+import Header from "./components/Header"
 
 function App() {
   return (
     <div className="container">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
