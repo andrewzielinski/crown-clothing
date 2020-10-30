c41ff9e4ec838e7782b6e1490e3fb4aa6f8b9dfa
Andrew Zielinski <andrew@2dev.com.au>
2020-10-30 17:38:06 +1100

    Store authenticated users in firebase db

--- src/pages/SignInAndSignUpPage.jsx
+++ src/pages/SignInAndSignUpPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './SignInAndSignUpPage.styles.scss';
 import SignIn from "../components/SignIn"
+import SignUp from "../components/SignUp"
+
 
 const SignInAndSignUpPage = () => {
   return (
     <div className="sign-in-and-sign-up-page">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
