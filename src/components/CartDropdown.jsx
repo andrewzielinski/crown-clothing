b648782f7b34a06c8ff3f5ab2f3fecbf0e390fea
Andrew Zielinski <andrew@2dev.com.au>
2020-11-01 20:32:17 +1100

    Added memoization for cart items and total item count

--- src/components/CartDropdown.jsx
+++ src/components/CartDropdown.jsx
@@ -3,6 +3,7 @@ import './CartDropdown.styles.scss'
 import Button from "./Button"
 import {connect} from "react-redux"
 import CartItem from "./CartItem"
+import {selectCartItems} from "../redux/cart/cart.selectors"
 
 const CartDropdown = ({ cartItems }) => {
   return (
@@ -18,8 +19,8 @@ const CartDropdown = ({ cartItems }) => {
 };
 
 
-const mapStateToProps = ({cart: { cartItems }}) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -4,11 +4,12 @@ import {toggleCartHidden} from '../redux/cart/Cart.actions'
 
 import './CartIcon.styles.scss'
 import { ReactComponent as ShoppingIcon } from "../assets/shopping-bag.svg"
+import {selectCartItemsCount} from "../redux/cart/cart.selectors"
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 )
 
@@ -16,7 +17,10 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state)
+})
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
 
 
