dab3806984cdf2a75906dfd542bf2c983dcc7c10
Andrew Zielinski <andrew@2dev.com.au>
2020-11-03 16:04:51 +1100

    Implemented redux-persist

--- src/index.js
+++ src/index.js
@@ -5,13 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
 import {Provider} from 'react-redux';
-import store from "./redux/Store"
+import { PersistGate } from 'redux-persist/integration/react';
+
+
+import {store, persistor} from "./redux/Store"
+
+
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App/>
+        <PersistGate persistor={persistor}>
+          <App/>
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>
